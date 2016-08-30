87d0ec7e76ebcdc0e0775c143ab0972ea56e244b
petr-tik <petr-tik@users.noreply.github.com>
2016-08-30 22:23:55 +0100

    changed the option flag in test

--- test_calc.sh
+++ test_calc.sh
@@ -1,5 +1,5 @@
-./income_calc -s 25000 -l UK
-./income_calc -s 35000 -l London -m
-./income_calc -m -l london -s 40000
-./income_calc -l uk -m -s 50000
-./income_calc -l Uk -s 160000 -m
+./income_calc -a 25000 -l UK
+./income_calc -a 35000 -l London -m
+./income_calc -m -l london -a 40000
+./income_calc -l uk -m -a 50000
+./income_calc -l Uk -a 160000 -m 
