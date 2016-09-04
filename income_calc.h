20b47e5958fb9c48c92cf75242f62088e88d5cb9
petr-tik <petr-tik@users.noreply.github.com>
2016-09-04 20:55:34 +0100

    add upper limit constants for tax_t type

--- income_calc.h
+++ income_calc.h
@@ -3,6 +3,9 @@
 
 // length of taxrates and salary_limits array. 
 #define NUM_TAXES 10
+#define MAX_FLOAT 1000000000
+// TODO - change the definition to real MAX_FLOAT
+#define MAX_PERC 100
 
 typedef struct tax_t {
   float TAXRATES[NUM_TAXES];
@@ -11,28 +14,31 @@ typedef struct tax_t {
 } tax_t;
 
 
-tax_t UK = {.TAXRATES = {0, 20, 40, 45}, 
-            .SALARY_LIMITS = {0, 11000, 43000, 150000},
+tax_t UK = {.TAXRATES = {0, 20, 40, 45, MAX_PERC}, 
+            .SALARY_LIMITS = {0, 11000, 43000, 150000, MAX_FLOAT},
             .SIGN = "£"};
 
-tax_t UK_NI = {.TAXRATES = {0, 12, 2},
-               .SALARY_LIMITS = {0, 8064, 43000},
+tax_t UK_NI = {.TAXRATES = {0, 12, 2, MAX_PERC},
+               .SALARY_LIMITS = {0, 8064, 43000, MAX_FLOAT},
                .SIGN = ""};
 
-tax_t California = {.TAXRATES = {1, 2, 4, 6, 8, 9.3, 10.3, 11.3, 12.3},
+tax_t California = {.TAXRATES = {1, 2, 4, 6, 8, 9.3, 10.3, 11.3, 12.3, MAX_PERC},
                     .SALARY_LIMITS = {0, 7850, 18610, 29372, 40773, 
-                                   51350, 263222, 315866, 526443, 1000000},
+                                   51350, 263222, 315866, 526443, 1000000, MAX_FLOAT},
                     .SIGN = "$"};
 
 tax_t Russia = {.TAXRATES = {13},
-                .SALARY_LIMITS = {100000000},
+                .SALARY_LIMITS = {MAX_FLOAT},
                 .SIGN = "RUB"};
 
 /* tax_t Seattle = {.TAXRATES = {},
                          .SALARY_LIMITS = {},
                          .SIGN = "$"};
 
-tax_t NYC
+tax_t NYC = {.TAXRATES = {},
+             .SALAY_LIMITS = {},
+             .SIGN = {}
+};
 */
 
 int calc_taxes(float *salary_ptr, float *taxes_paid, tax_t tax_rules);
