3cc2081be30828d78d5807e895c62486cfe79491
petr-tik <petr-tik@users.noreply.github.com>
2016-09-08 08:59:20 +0100

    added wrong tests

--- test_calc.sh
+++ test_calc.sh
@@ -3,3 +3,8 @@
 ./income_calc -m -l london -a 40000
 ./income_calc -l uk -m -a 50000
 ./income_calc -l Uk -a 160000 -m 
+# wrong examples
+./income_calc -l sd -a sdf
+./income_calc -l nyc -m
+./income_calc -a 25000
+./income_calc -a 150000 -m
