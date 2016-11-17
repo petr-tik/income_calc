e9c885d210b86c19799afba20a24df5b642cb84e
petr-tik <petr-tik@users.noreply.github.com>
2016-11-17 22:35:34 +0000

    autopep8

--- download_stock_quotes.py
+++ download_stock_quotes.py
@@ -1,23 +1,23 @@
 #! /usr/bin/env python
 """
-Script to download prices of different companies' stock 
-used to calculate the value of shares in an offer. 
-Ran during make by downloading a csv of Stock Prices from Yahoo.Finance 
-for most popular tech companies (list below). 
-Ran from makefile, so it prints to stdout, rather than logs. 
+Script to download prices of different companies' stock
+used to calculate the value of shares in an offer.
+Ran during make by downloading a csv of Stock Prices from Yahoo.Finance
+for most popular tech companies (list below).
+Ran from makefile, so it prints to stdout, rather than logs.
 
-If the OPTION_FLAGS determines the format of the csv, which is later read by a function in C. 
+If the OPTION_FLAGS determines the format of the csv, which is later read by a function in C.
 
 TODO: add tests to assert that all relevant information is always present. If you change the flags - need to rerun the tests.
 
 Running:
 
-the script is run without args during installation and at any other time, 
+the script is run without args during installation and at any other time,
 when you a new file needs to be created
 
 python download_stock_quotes.py
 
-When a stock_quote is missing from the file, run the script 
+When a stock_quote is missing from the file, run the script
 with sys.argv[2] = stock_quote
 
 python download_stock_quotes.py $STOCK_QUOTE
@@ -32,7 +32,7 @@ import os.path
 
 TIMEOUT_VAL = 10
 
-STOCK_QUOTES = ["AAPL", "MSFT", "INTC", "IBM", "CSCO", "ORCL", "GOOG", 
+STOCK_QUOTES = ["AAPL", "MSFT", "INTC", "IBM", "CSCO", "ORCL", "GOOG",
                 "FB", "NVDA", "YHOO", "LNKD", "TWTR", "YNDX"]
 # TODO - include MAIL.RU stock index
 
@@ -41,22 +41,24 @@ STOCK_QUOTES = ["AAPL", "MSFT", "INTC", "IBM", "CSCO", "ORCL", "GOOG",
 # http://www.jarloo.com/yahoo_finance/
 OUTPUT_FILENAME = ".stocks.csv"
 
+
 def make_url_for_stocks(new_stock_quote=None):
     """ Fills a yahoo finance URL with option flags and stock quotes """
     STOCK_QUOTES_STR = join(STOCK_QUOTES, "+")
-    if new_stock_quote != None and new_stock_quote not in STOCK_QUOTES:
+    if new_stock_quote is not None and new_stock_quote not in STOCK_QUOTES:
         STOCK_QUOTES_STR = new_stock_quote
     OPTION_FLAGS = "nsm3"
     URL = "http://download.finance.yahoo.com/d/quotes.csv\
     ?s={}&f={}".format(STOCK_QUOTES_STR, OPTION_FLAGS)
     return URL
 
+
 def download_stock_prices(url, open_method, timeout=TIMEOUT_VAL):
-    """ Takes a URL with filled-in option flags and stock_quotes 
-    and saves it to a csv file. 
+    """ Takes a URL with filled-in option flags and stock_quotes
+    and saves it to a csv file.
     Open_methods:
         write - if no file exists
-        append - if file exists and stock_quote is new. 
+        append - if file exists and stock_quote is new.
                  adds a row with the info of that stock quote
     """
     print "Downloading stock data"
@@ -72,8 +74,8 @@ def download_stock_prices(url, open_method, timeout=TIMEOUT_VAL):
         print "Success! Data saved under {}".format(OUTPUT_FILENAME)
         return True
     except requests.HTTPError as e:
-        print "Error! Checking connection failed, status code {}.".format(\
-                                                    e.response.status_code)
+        print "Error! Checking connection failed, status code {}.".format(
+            e.response.status_code)
     except requests.ConnectionError:
         print "Error! No internet connection available."
     return False
@@ -82,8 +84,8 @@ def download_stock_prices(url, open_method, timeout=TIMEOUT_VAL):
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         open_method = "w"
-        URL = make_url_for_stocks()        
-    elif len(sys.argv) == 2 and os.path.exists(OUTPUT_FILENAME): 
+        URL = make_url_for_stocks()
+    elif len(sys.argv) == 2 and os.path.exists(OUTPUT_FILENAME):
         open_method = "a"
         URL = make_url_for_stocks(sys.argv[1])
     else:
