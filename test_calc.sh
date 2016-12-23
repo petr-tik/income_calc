61cec381c49c580c8dd445739047f48c782b204f
petr-tik <petr-tik@users.noreply.github.com>
2016-12-23 23:55:48 +0000

    Adapt testing script for artefact 2
    income_calc_test2 only outputs 2 decimal point floats - makes it easy to test

--- test_calc.sh
+++ test_calc.sh
@@ -1,10 +1,10 @@
-./income_calc -a 25000 -l UK
-./income_calc -a 35000 -l London -m
-./income_calc -m -l london -a 40000
-./income_calc -l uk -m -a 50000
-./income_calc -l Uk -a 160000 -m 
+./income_calc_test2 -a 25000 -l UK
+./income_calc_test2 -a 35000 -l London -m
+./income_calc_test2 -m -l london -a 40000
+./income_calc_test2 -l uk -m -a 50000
+./income_calc_test2 -l Uk -a 160000 -m 
 # wrong examples
-./income_calc -l sd -a sdf
-./income_calc -l nyc -m
-./income_calc -a 25000
-./income_calc -a 150000 -m
+./income_calc_test2 -l sd -a sdf
+./income_calc_test2 -l nyc -m
+./income_calc_test2 -a 25000
+./income_calc_test2 -a 150000 -m
